a733c2aa337bd4c81ad1586f97839218fb171f60
Петросян Ваган <vagan.petrosyan@gazprombank.ru>
2022-12-15 14:17:00 +0300

    add react-hook-form

--- react/src/components/Selections/CreateSelectionForm.js
+++ react/src/components/Selections/CreateSelectionForm.js
@@ -1,58 +1,39 @@
 import { useDispatch } from 'react-redux'
-import {useState} from "react";
-import {isObjectEmpty, isStringEmpty} from "../../utils/utils";
+import {useForm} from 'react-hook-form'
 
 function CreateSelectionForm() {
   const dispatch = useDispatch()
-  const [selectionName, setSelectionName] = useState("")
-  const [selectionAuthor, setSelectionAuthor] = useState("")
-  const [selectionEmail, setSelectionEmail] = useState("")
 
-  const [errors, setErrors] = useState({})
-  const onSubmit = async () => {
-    let errors = {}
-    if (isStringEmpty(selectionName)) {
-      errors.selectionName = 'required'
-    }
-    if (isStringEmpty(selectionAuthor)) {
-      errors.selectionAuthor = 'required'
-    }
-    if (isObjectEmpty(errors)) {
-      dispatch({ type: "CREATE_SELECTION", payload: {
-        title: selectionName,
-        author: selectionAuthor,
-        email: selectionEmail
-      }})
-      setSelectionName("")
-      setSelectionAuthor("")
-      setSelectionEmail("")
-      setErrors({})
-    } else {
-      setErrors(errors)
-    }
+  const { register, handleSubmit, formState: errors } = useForm()
+
+  const onSubmit = async ({selectionAuthor, selectionEmail, selectionName}) => {
+    debugger
+    dispatch({ type: "CREATE_SELECTION", payload: {
+      title: selectionName,
+      author: selectionAuthor,
+      email: selectionEmail
+    }})
   }
 
   return (
     <div className="create_selection_form_wrapper">
-      <form className="create_selection_form row" onSubmit={e=>{e.preventDefault(); onSubmit()}}>
+      <form className="create_selection_form row" onSubmit={handleSubmit(onSubmit)}>
         <div className="create_selection_input col-md-4" >
           <label htmlFor="selectionName" className="form-label">Selection Title</label>
-          <input type="text" className="form-control" id="selectionName" value={selectionName}
-                 onChange={e=>setSelectionName(e.target.value)} />
-          {errors.selectionName && <span className="form_error">This field is required</span>}
+          <input type="text" className="form-control" id="selectionName" name="selectionName" {...register('selectionName', { required: true, maxLength: 10 })} />
+          {errors?.selectionName && <span className="form_error">{errors.selectionName}</span>}
         </div>
         <div className="create_selection_input col-md-4">
           <label htmlFor="selectionAuthor" className="form-label">Selection Author</label>
-          <input type="text" className="form-control" id="selectionAuthor" value={selectionAuthor}
-                 onChange={e=>setSelectionAuthor(e.target.value)} />
-          {errors.selectionAuthor && <span className="form_error">This field is required</span>}
+          <input type="text" className="form-control" id="selectionAuthor" {...register('selectionAuthor', { required: true })} />
+          {errors?.selectionAuthor && <span className="form_error">{errors.selectionAuthor}</span>}
         </div>
         <div className="create_selection_input col-md-4">
-          <label htmlFor="selectionAuthor" className="form-label">E-mail</label>
-          <input type="text" className="form-control" id="selectionAuthor" value={selectionEmail}
-                 onChange={e=>setSelectionEmail(e.target.value)} />
-          {errors.selectionEmail && <span className="form_error">This field is required</span>}
+          <label htmlFor="selectionEmail" className="form-label">E-mail</label>
+          <input type="text" className="form-control" id="selectionEmail" {...register('selectionEmail', { required: false })} />
+          {errors?.selectionEmail && <span className="form_error">{errors.selectionEmail}</span>}
         </div>
+        {JSON.stringify(errors)}
         <div className="create_selection_form_add_btn_wrapper">
           <button type="submit" className="btn btn-primary">Create selection</button>
         </div>
